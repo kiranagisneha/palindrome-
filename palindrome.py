48158c6fae17640e08eebb5938614f69458a836c
kiranagisneha <kiranagisneha@gmail.com>
2025-11-18 18:54:18 +0530

    save

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,18 @@
 import sys
-if len(sys.argv) == 2:
-    text = sys.argv[1] 
-    print("User provided input:")
-else:
-    print("No command-line input provided. Using manual input:")
-    text = input("Enter a string: ")  
+
+try:
+    # If argument is given, use it
+    if len(sys.argv) >= 2:
+        text = sys.argv[1]
+        print("User provided input:")
+    else:
+        # Try manual input
+        print("No command-line input provided. Using manual input:")
+        text = input("Enter a string: ")
+
+except EOFError:
+    print("No input available. Setting default value.")
+    text = "" 
 if text == text[::-1]:
     print("It is a palindrome.")
 else:
